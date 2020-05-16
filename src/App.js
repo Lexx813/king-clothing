869035d27a5df813b4d5f949807fb3d5e88a0931
lexx813 <lexxdev@LexxDev.localdomain>
2020-05-16 11:51:42 -0500

    added collection item and preview

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <div className='App'>
          <Switch>
             <Route exact path='/' component={HomePage} />
+            <Route exact path='/shop' component={ShopPage} />
          </Switch>
       </div>
    );
