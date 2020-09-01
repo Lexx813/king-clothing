9723984d312add2ccba51ace8bd4d41a0df25ff6
lexx813 <lexxsolutionz@gmail.com>
2020-09-01 10:30:05 -0500

    added selectors for check out page

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -1,20 +1,27 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
-const selectCart = state => state.cart;
+const selectCart = (state) => state.cart;
 
 export const selectCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems 
+  [selectCart],
+  (cart) => cart.cartItems,
 );
-
 
 export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems =>
+  [selectCartItems],
+  (cartItems) =>
     cartItems.reduce(
-        (accumaltedQuantity, cartItem) =>
-        accumaltedQuantity + cartItem.quantity,
-        0
-    )
+      (accumaltedQuantity, cartItem) => accumaltedQuantity + cartItem.quantity,
+      0,
+    ),
 );
 
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumaltedQuantity, cartItem) =>
+        accumaltedQuantity + cartItem.quantity * cartItem.price,
+      0,
+    ),
+);
