155d15b070ff9658dd61a50be0ae94751d38be80
lexx813 <lexxsolutionz@gmail.com>
2022-06-05 06:29:48 -0500

    migrating to redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
-import { CartProvider } from "./contexts/cart.context";
+
 import { store } from "./store/store";
 
 import "./index.scss";
@@ -17,11 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,15 +1,16 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, Fragment } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import './category.styles.scss';
+import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -21,7 +22,7 @@ const Category = () => {
       <h2 className='category-title'>{category.toUpperCase()}</h2>
       <div className='category-container'>
         {products &&
-          products.map((product) => (
+          products.map(product => (
             <ProductCard key={product.id} product={product} />
           ))}
       </div>
