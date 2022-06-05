eb7ff05bef36da8506f6f8d8a7de05e679909eb3
lexx813 <lexxsolutionz@gmail.com>
2022-06-05 08:07:49 -0500

    added persist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+
+import { Routes, Route } from "react-router-dom";
 
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "./utils/firebase/firebase.utils";
-
 import Home from "./routes/home/home.component";
-import Navigation from "./components/navigation/navigation.component";
+import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
 import Shop from "./routes/shop/shop.component";
-import Blog from "./routes/blog/blog.component";
-import CheckOut from "./routes/checkout/checkout.component";
+import Checkout from "./routes/checkout/checkout.component";
 import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(user => {
       if (user) {
@@ -28,14 +27,14 @@ const App = () => {
 
     return unsubscribe;
   }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='/shop/*' element={<Shop />} />
-        <Route path='/auth' element={<Authentication />} />
-        <Route path='/checkout' element={<CheckOut />} />
-        <Route path='/blog' element={<Blog />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
