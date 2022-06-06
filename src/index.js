b6450428858cab3458ac47aa165111ccd4ae41c9
lexx813 <lexxsolutionz@gmail.com>
2022-06-06 06:14:25 -0500

    added stripe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
-
 import { store, persistor } from "./store/store";
-
+import { stripePromise } from "./utils/stripe/stripe.utils";
 import "./index.scss";
 
 const container = document.getElementById("app");
@@ -17,7 +17,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
